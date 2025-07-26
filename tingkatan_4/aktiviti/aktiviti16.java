061046b1328d9233bf40fe05e372c50bb1fb42f2
luq-mne <luqman.yazrie@outlook.com>
2025-07-26 19:46:50 +0800

    aktiviti 16

---
+++ tingkatan_4/aktiviti/aktiviti16.java
@@ -0,0 +1,9 @@
+package tingkatan_4.aktiviti;
+
+public class aktiviti16 {
+    public static void main(String[] args) {
+        int pengunjung_ke_malaysia = 16500200;
+        int tinggi = 199;
+        char abjad = 'L';
+    }
+}
