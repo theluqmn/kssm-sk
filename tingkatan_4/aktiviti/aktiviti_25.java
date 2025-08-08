bebfd6ff213b2d2ec3e6df1bf365060ec9ff1955
luq-mne <luqman.yazrie@outlook.com>
2025-08-08 22:13:27 +0800

    aktiviti 25

---
+++ tingkatan_4/aktiviti/aktiviti_25.java
@@ -0,0 +1,12 @@
+// buku teks m/s 121
+
+package tingkatan_4.aktiviti;
+
+public class aktiviti_25 {
+    public static void main(String[] args) {
+        int Kilometer = 20;
+        int meter;
+
+        System.out.println("\n" + Kilometer + " Kilometer");
+    }
+}
